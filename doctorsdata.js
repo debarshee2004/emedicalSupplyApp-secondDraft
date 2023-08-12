3e18b377b2c6337c1f924fc2040b8b8b82db3568
Debarshee Chakraborty <129538241+debarshee2004@users.noreply.github.com>
2023-08-12 22:28:59 +0530

    data about doctors and products

---
+++ doctorsdata.js
@@ -0,0 +1,46 @@
+// doctor.data.js
+
+const doctorData = [
+  {
+    "name": "Dr. John Smith",
+    "specialty": "Cardiology",
+    "location": "New York, NY",
+    "rating": 4.9,
+    "experience": "15+ years",
+    "languages": ["English", "Spanish"]
+  },
+  {
+    "name": "Dr. Emily Johnson",
+    "specialty": "Pediatrics",
+    "location": "Los Angeles, CA",
+    "rating": 4.8,
+    "experience": "12+ years",
+    "languages": ["English"]
+  },
+  {
+    "name": "Dr. Ahmed Khan",
+    "specialty": "Orthopedics",
+    "location": "Chicago, IL",
+    "rating": 4.7,
+    "experience": "10+ years",
+    "languages": ["English", "Arabic"]
+  },
+  {
+    "name": "Dr. Maria Rodriguez",
+    "specialty": "Dermatology",
+    "location": "Miami, FL",
+    "rating": 4.6,
+    "experience": "8+ years",
+    "languages": ["English", "Spanish"]
+  },
+  {
+    "name": "Dr. David Lee",
+    "specialty": "Ophthalmology",
+    "location": "San Francisco, CA",
+    "rating": 4.9,
+    "experience": "14+ years",
+    "languages": ["English", "Mandarin"]
+  }
+];
+
+export default doctorData;
---
+++ productsdata.js
@@ -0,0 +1,86 @@
+// product.data.js
+
+const productData = [
+  {
+    "name": "Aspirin",
+    "category": "Pain Relief",
+    "price": 5.99,
+    "stock": 150,
+    "description": "Relieves minor aches and pains.",
+    "manufacturer": "Johnson & Johnson"
+  },
+  {
+    "name": "Ibuprofen",
+    "category": "Pain Relief",
+    "price": 7.49,
+    "stock": 200,
+    "description": "Reduces fever and provides pain relief.",
+    "manufacturer": "Pfizer"
+  },
+  {
+    "name": "Cetrizine",
+    "category": "Allergy",
+    "price": 9.99,
+    "stock": 100,
+    "description": "Relieves allergy symptoms like sneezing and runny nose.",
+    "manufacturer": "Novartis"
+  },
+  {
+    "name": "Amoxicillin",
+    "category": "Antibiotics",
+    "price": 12.95,
+    "stock": 50,
+    "description": "Treats bacterial infections.",
+    "manufacturer": "GSK"
+  },
+  {
+    "name": "Loratadine",
+    "category": "Allergy",
+    "price": 8.25,
+    "stock": 120,
+    "description": "Provides non-drowsy relief from allergy symptoms.",
+    "manufacturer": "Bayer"
+  },
+  {
+    "name": "Omeprazole",
+    "category": "Digestive Health",
+    "price": 11.50,
+    "stock": 80,
+    "description": "Reduces stomach acid and treats heartburn.",
+    "manufacturer": "AstraZeneca"
+  },
+  {
+    "name": "Paracetamol",
+    "category": "Pain Relief",
+    "price": 4.75,
+    "stock": 250,
+    "description": "Relieves mild to moderate pain and reduces fever.",
+    "manufacturer": "GlaxoSmithKline"
+  },
+  {
+    "name": "Vitamin C",
+    "category": "Vitamins",
+    "price": 6.99,
+    "stock": 180,
+    "description": "Boosts immune system and supports overall health.",
+    "manufacturer": "Nature's Way"
+  },
+  {
+    "name": "Hydrocortisone Cream",
+    "category": "Skin Care",
+    "price": 8.95,
+    "stock": 70,
+    "description": "Relieves itching, redness, and inflammation of the skin.",
+    "manufacturer": "Eucerin"
+  },
+  {
+    "name": "Thermometer",
+    "category": "Medical Supplies",
+    "price": 15.00,
+    "stock": 30,
+    "description": "Measures body temperature accurately.",
+    "manufacturer": "Omron"
+  }
+];
+
+export default productData;
